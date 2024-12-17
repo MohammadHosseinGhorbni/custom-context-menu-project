f8a418b428f61552bf4c1d6a2c3f04cf81f1023a
Hossein <hoseing211@gmail.com>
2024-12-17 20:08:57 +0330

    Add files via upload

---
+++ custom-context-menu-project/150-explain-of-custom-context-menu-project/source/js/script.js
@@ -0,0 +1,28 @@
+﻿let contextMenuElem = document.querySelector("#contextMenu");
+// console.log(contextMenuElem);
+
+function contextHandler(event) {
+  event.preventDefault();
+  if (contextMenuElem.style.display === "none") {
+    contextMenuElem.style.left = event.pageX + "px";
+    contextMenuElem.style.top = event.pageY + "px";
+    contextMenuElem.style.display = "block";
+  } else {
+    contextMenuElem.style.left = event.pageX + "px";
+    contextMenuElem.style.top = event.pageY + "px";
+  }
+}
+
+function clickHandler() {
+  contextMenuElem.style.display = "none";
+}
+
+function keyUpHandler(event) {
+  if (event.keyCode === 27) {
+    contextMenuElem.style.display = "none";
+  }
+}
+
+document.body.addEventListener("contextmenu", contextHandler);
+document.body.addEventListener("click", clickHandler);
+document.body.addEventListener("keyup", keyUpHandler);
